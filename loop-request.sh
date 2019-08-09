d9ab0658f07eb0ab6e3a6d251ce6c86bd7798736
Rodrigo Conceição de Araujo <rodrigo.araujo@southsystem.com.br>
2019-08-09 09:26:42 -0300

    Created script to generate requests

---
+++ loop-request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while :
+do
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+done
