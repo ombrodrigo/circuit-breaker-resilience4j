7168bca4ce9d5113ef4cc49bbfdb153804387da5
Rodrigo Conceição de Araujo <omb.rodrigo@gmail.com>
2019-08-09 13:54:13 -0300

    Update loop

--- loop-request.sh
+++ loop-request.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+NUMBER_REPETITIONS=10
+URL="http://localhost:8080/externalRequest/date-time"
+
 while :
 do
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
-   curl -XGET http://localhost:8080/externalRequest/date-time &> /dev/null 
+	for (( i=0; i <= $NUMBER_REPETITIONS; ++i ))
+	do
+	    curl -XGET $URL &> /dev/null
+	done
 done
